a6abf0564bd2b90aa596f42ad62a14f6b4f3ebaf
youngwooKim <rladyddn258@gmail.com>
2025-08-30 15:43:42 +0900

    기본 설정 세팅

---
+++ src/main/kotlin/com/shipal/shipal/ShipalApplication.kt
@@ -0,0 +1,11 @@
+package com.shipal.shipal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShipalApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShipalApplication>(*args)
+}
---
+++ src/test/kotlin/com/shipal/shipal/ShipalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shipal.shipal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShipalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
